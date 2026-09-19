a99215db309e423f04a4807f11b47ac869c00972
Adebiyi Qudus <adebiyiqudus2018@gmail.com>
2026-09-19 23:45:47 +0100

    Context API update

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 // Archived posts are posts that have been moved out of the main/current list but kept somewhere else for storage or later access.
+// Context API is a way to share values or data between components without having to explicitly pass a prop through every level of the tree. 
 // The Provider component is used to wrap the components that need access to the context value, and it takes a value prop that specifies the value to be provided to the child components.
 // Value is the data that you want to share with the components that are wrapped by the Provider. In this case, we're providing the `posts`, `onAddPost`, `onClearPosts`, `searchQuery`, and `setSearchQuery` values to the child components.
 // Consumer components are the components that need access to the context value. They can use the useContext hook to consume or receive the context value provided by the Provider component.
